2bc6dbe29ba7f7fde8ba08dfa304cc41d3167869
rhyang <92644438+libehaner@users.noreply.github.com>
2021-11-04 03:19:19 +0800

    Add files via upload

---
+++ main100_extractHeadMotion/Main_extract_HeadMotion_OnTrials.m
@@ -0,0 +1,60 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%              Head motion correlation               %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Load data
+clear;close all;clc;
+disp('Step 1: Load data and parameter setting');
+RandStream('mt19937ar','seed',1000);
+load('datafordeception.mat');
+clear Luo Luo2  Regions i datalength ans subject_list
+clear rTPJ BA10 earnings iqid roi roinames
+clear con inc strat
+
+%%% % % % % % % % % % % % % % % % % discard subject in outliers
+outlier = [43,75];
+BigMotion(outlier) = [];
+bigdata(outlier) = [];
+
+clear idx outlier roi iq inds 
+
+% % % % % % % % % % % % % % % % % % % % % extract event onset (in IndSeq)
+nSub = length(BigMotion);
+nTrial = 60;
+IndSeq = cell(nSub,1);
+for subj = 1:nSub
+for j = 1 : nTrial
+    if bigdata{subj}.Timing(1).time(j) > floor(bigdata{subj}.Timing(1).time(j))
+        IndSeq{subj}(j,1) = floor(bigdata{subj}.Timing(1).time(j)) + 1;
+    else
+        IndSeq{subj}(j,1) = bigdata{subj}.Timing(1).time(j);  % in TRs
+    end
+    IndSeq{subj}(j,2) = floor(bigdata{subj}.Timing(2).time(j));
+    onset{subj}.sot{1}(j,1) = bigdata{subj}.Timing(1).time(j); % in TRs
+end
+end
+clear onset j bigdata 
+
+% % % % % % % % % % % % % % % % % % % Extract Head motion at trials level
+
+
+HM_displamnt = nan(nSub,60,2);
+HM_frameWise = nan(nSub,60,2);
+for subj = 1:nSub
+    tmp = BigMotion(subj).motion;
+    tmpDiff = [tmp(1,:);diff(tmp)];
+    for j = 1:60
+        idx = IndSeq{subj}(j,1):IndSeq{subj}(j,2);
+        HM_displamnt(subj,j,1) = max(sqrt(sum(tmp(idx,1:3).^2,2)));
+        HM_displamnt(subj,j,2) = max(sqrt(sum(tmp(idx,4:6).^2,2)))*180/3.14;
+        HM_frameWise(subj,j,1) = max(sqrt(sum(tmpDiff(idx,1:3).^2,2)));
+        HM_frameWise(subj,j,2) = max(sqrt(sum(tmpDiff(idx,4:6).^2,2)))*180/3.14;
+    end
+end
+HM_displamnt(:,1,:) = [];
+HM_frameWise(:,1,:) = [];
+
+clear  subj i j k  BigMotion
+name = {'Transition','Rotation'};
+save('../resultData/HeadMotion','HM_displamnt','HM_frameWise','name');
+save('./HeadMotion','HM_displamnt','HM_frameWise','name');
+disp('ok ... ...')
---
+++ main100_extractHeadMotion/Main_headMotion2weight.m
@@ -0,0 +1,69 @@
+% % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %  create weight on trial according Head motion
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% % % Load data
+clear;close all;clc;
+load('datafordeception.mat');
+clear Luo Luo2  Regions i datalength ans subject_list roi
+clear BA10 inds iqid rTPJ roinames iq
+
+% % % ---------------------------------------------% % %
+% % % % discard subjects in outliers
+clear con inc strat
+outlier = [43,75];
+bigdata(outlier) = [];
+BigMotion(outlier) = [];
+clear idx outlier roi
+nSub = length(bigdata);
+nTrial = 60;
+
+% % % ---------------------------------------------% % %
+% %%  Thinking time distribution
+disp('Step 2: Thinking time distribution');
+IndSeq = cell(nSub,1);
+onset  = cell(nSub,1);
+thinkingtime = nan(nSub,nTrial);
+for i = 1 : nSub
+    IndSeq{i,1} = zeros(nTrial,2);
+    for j = 1 : nTrial
+        if bigdata{i}.Timing(1).time(j) > floor(bigdata{i}.Timing(1).time(j))
+            IndSeq{i}(j,1) = floor(bigdata{i}.Timing(1).time(j)) + 1;
+        else
+            IndSeq{i}(j,1) = bigdata{i}.Timing(1).time(j);  % in TRs
+        end
+        IndSeq{i}(j,2) = floor(bigdata{i}.Timing(2).time(j));
+        thinkingtime(i,j) = IndSeq{i}(j,2) - IndSeq{i}(j,1)+1;
+        onset{i}.sot{1}(j,1) = bigdata{i}.Timing(1).time(j); % in TRs
+    end
+end
+clear thinkingtime onset
+
+% % % -------------------------------% % %
+% %% Extract Head Motion
+tmove = cell(nSub,1);
+for subj = 1 : nSub
+    temp = [BigMotion(subj).motion(1,:); BigMotion(subj).motion];
+    temp = zscore(temp); % rotation may be scaled differently against location
+    tmove{subj} = temp(1:end-1,:);
+end
+
+% % % ---------------------------------------------% % %
+newExclude = zeros(nSub,nTrial);
+for subj = 1:nSub
+    move = tmove{subj};
+    for iTrail = 1 : size(IndSeq{subj},1)
+        indexTrail = IndSeq{subj}(iTrail,1) : IndSeq{subj}(iTrail,2);
+        tmpMove = [sqrt(sum(diff(move(indexTrail,1:3)).^2,2)),...
+            sqrt(sum(diff(move(indexTrail,4:6)).^2,2))];
+        newExclude(subj,iTrail) = max(tmpMove(:));
+    end
+end
+
+% my_pcolor(newExclude>2);
+exclude = newExclude;
+save('exclude','exclude');
+save('../main101_code4GCA_fMRI/exclude','exclude');
+clear i j iTrail subj nTrial nSub
+disp('The End ... ...')
